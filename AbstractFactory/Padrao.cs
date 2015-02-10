9a443c5df2876286de88861c258ae10b0fb0b84d
Rafael Orion <rafaelorion@gmail.com>
2015-02-10 19:54:01 -0200

    inicial

---
+++ AbstractFactory/Padrao.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Creational.AbstractFactory
+{
+    /// <summary>
+    /// MainApp startup class for Structural
+    /// Abstract Factory Design Pattern.
+    /// </summary>
+    class MainApp
+    {
+        public static void Main2()
+        {
+            // Abstract factory #1
+            AbstractFactory factory1 = new ConcreteFactory1();
+            Client client1 = new Client(factory1);
+            client1.Run();
+
+            // Abstract factory #2
+            AbstractFactory factory2 = new ConcreteFactory2();
+            Client client2 = new Client(factory2);
+            client2.Run();
+
+            // Wait for user input
+            Console.ReadKey();
+        }
+    }
+
+    abstract class AbstractFactory
+    {
+        public abstract AbstractProductA CreateProductA();
+        public abstract AbstractProductB CreateProductB();
+    }
+    class ConcreteFactory1 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA1();
+        }
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB1();
+        }
+    }
+    class ConcreteFactory2 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA2();
+        }
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB2();
+        }
+    }
+    abstract class AbstractProductA
+    {
+    }
+    abstract class AbstractProductB
+    {
+        public abstract void Interact(AbstractProductA a);
+    }
+    class ProductA1 : AbstractProductA
+    {
+    }
+    class ProductB1 : AbstractProductB
+    {
+        public override void Interact(AbstractProductA a)
+        {
+            Console.WriteLine(this.GetType().Name +
+              " interacts with " + a.GetType().Name);
+        }
+    }
+    class ProductA2 : AbstractProductA
+    {
+    }
+
+    class ProductB2 : AbstractProductB
+    {
+        public override void Interact(AbstractProductA a)
+        {
+            Console.WriteLine(this.GetType().Name +
+              " interacts with " + a.GetType().Name);
+        }
+    }
+
+    class Client
+    {
+        private AbstractProductA _abstractProductA;
+        private AbstractProductB _abstractProductB;
+
+        public Client(AbstractFactory factory)
+        {
+            _abstractProductB = factory.CreateProductB();
+            _abstractProductA = factory.CreateProductA();
+        }
+
+        public void Run()
+        {
+            _abstractProductB.Interact(_abstractProductA);
+        }
+    }
+}
